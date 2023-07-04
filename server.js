f526a8a567dc3065b3a86ba03a541efc3ea336fc
Michael <michael.riordan81@outlook.com>
2023-07-03 21:37:46 -0700

    create mySQL database using mySQL workbench. Query mySQL + appointment tests

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const connection = require('./db');
 const app = express();
 require('dotenv').config();
 const port = 8000;
@@ -14,6 +16,8 @@ function logger(req, res, next) {
 app.use(logger);
 
 app.use(cors());
+
+app.use(bodyParser.json());
 
 app.get('/api/places', async (req, res) => {
     const apiKey = process.env.PLACES_API_KEY;
@@ -38,6 +42,21 @@ app.get('/api/distance', async (req, res) => {
         res.status(500).json({ error: 'Most likely cause being distance not yet set' });
     }
 });
+
+app.get('/appointment', (req, res) => {
+    res.send('list of all appointments');
+})
+
+app.post('/addAppointment', (req, res) => {
+    const ADD_QUERY = `insert into notaryappointmentmanager.appointments (appointment) values ('${req.body.appointment}')`
+    connection.query(ADD_QUERY, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send('appointment has been added');
+        }
+    })
+})
 
 
 /* EIA api call if needed in future. (tracks cost of gasoline in PADD 5 region)
