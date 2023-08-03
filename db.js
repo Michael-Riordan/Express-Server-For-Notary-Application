ebed55b5c209d960a966822935e26ea6ac7d3df8
Michael <michael.riordan81@outlook.com>
2023-08-02 22:31:13 -0700

    add port to connectionPool for rds connection

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const connectionPool = mysql.createPool({
     connectionLimit: 5,
     host: `${process.env.AMAZON_RDS_ENDPOINT}`,
+    port: 3306,
     user: `${process.env.RDS_USERNAME}`,
     password: `${process.env.MYSQL_PASSWORD}`,
     database: 'notaryappointmentmanager',
